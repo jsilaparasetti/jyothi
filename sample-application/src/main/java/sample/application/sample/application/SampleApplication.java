0b8960d2ff7dbf1874d1a8f2a776f2ff78d32712
jsilaparasetti <117080611+jsilaparasetti@users.noreply.github.com>
2022-12-13 20:22:41 +0530

    Update SampleApplication.java

--- sample-application/src/main/java/sample/application/sample/application/SampleApplication.java
+++ sample-application/src/main/java/sample/application/sample/application/SampleApplication.java
@@ -1,15 +1,13 @@
-package sample.application.sample.application;
-
+package com.example.eureka.server.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@EnableDiscoveryClient
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class SampleApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
-	}
-
+public static void main(String[] args) {
+SpringApplication.run(EurekaServerApplication.class, args);
+}
 }
